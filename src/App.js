fdef7016cd72107e577328e4e061a813060cbc6f
Guglielmo Turco <guglielmo.turco@gmail.com>
2018-08-22 23:43:15 -0700

    Refactoring and modification for firt implementation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+
+
 
 class App extends Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     error: null,
+  //     isLoaded: false,
+  //     nodes: []
+  //   }
+  // }
+
+  // requestNodes = (n) => {
+  //   getNodes(n)
+  //   .then( result => {
+  //       this.setState({
+  //         isLoaded: true,
+  //         nodes: result.data.configurations
+  //       });}
+  //
+  //   )
+  //   .catch( error => {
+  //       // Error
+  //
+  //       if (error.response) {
+  //           // The request was made and the server responded with a status code
+  //           // that falls out of the range of 2xx
+  //           // console.log(error.response.data);
+  //           console.log(error.response.status);
+  //           // console.log(error.response.headers);
+  //       } else if (error.request) {
+  //           // The request was made but no response was received
+  //           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+  //           // http.ClientRequest in node.js
+  //           console.log(error.request);
+  //       } else {
+  //           // Something happened in setting up the request that triggered an Error
+  //           console.log('Error', error.message);
+  //       }
+  //       console.log(error.config);
+  //     this.setState({
+  //       isLoaded: true,
+  //       error: error.message
+  //     });
+  //   });
+  // }
+
+  // componentDidMount(){
+  //   this.requestNodes(1000);
+  // }
+
+
   render() {
+    // const { nodes, error, isLoaded } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Nessus">
+        <Header />
+        <Main className="App-intro"/>
       </div>
     );
   }
