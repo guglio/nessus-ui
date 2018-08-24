426add82286ed6e8431a56bf89e7533b70734d20
Guglielmo <guglielmo.turco@allscripts.com>
2018-08-23 20:40:51 -0700

    Table to display all data

--- src/components/nodes-table/NodesTable.js
+++ src/components/nodes-table/NodesTable.js
@@ -1,60 +1,119 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
+
+import TableHeader from '../../containers/TableHeader';
+import TableContent from '../../containers/TableContent';
+
+import { ORDER, TABLE_HEADER } from '../../constants/';
 
 const styles = theme => ({
   root: {
     width: '100%',
     marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
   },
   table: {
-    minWidth: 700,
-  },
+    tableLayout: 'fixed'
+  }
 });
 
+class NodesTable extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      page: 0,
+      rowsPerPage: 5,
+      order: ORDER.ASC,
+      orderBy: TABLE_HEADER[0].value,
+      nodes:this.props.nodes,
+    }
+  }
 
-const NodesTable = (props) => {
-  const { classes, nodes } = props;
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
 
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Node name</TableCell>
-            <TableCell>Hostname</TableCell>
-            <TableCell>Username</TableCell>
-            <TableCell numeric>Port</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {nodes.map((n,i) => {
-            return (
-              <TableRow key={i}>
-                <TableCell component="th" scope="row">
-                  {n.name}
-                </TableCell>
-                <TableCell>{n.hostname}</TableCell>
-                <TableCell>{n.username}</TableCell>
-                <TableCell numeric>{n.port}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+
+  handleRequestSort = (event, property) => {
+   const orderBy = property;
+   const { ASC, DESC } = ORDER;
+   let order = DESC;
+
+   if (this.state.orderBy === property && this.state.order === DESC) {
+     order = ASC;
+   }
+
+   this.setState({ order, orderBy });
+ };
+
+
+  sortNodes = (orderBy) => {
+    let order = '';
+    const { ASC, DESC } = ORDER;
+
+    switch(this.state.order){
+      case ASC: order = DESC; break;
+      case DESC: order = ASC; break;
+      default: order = ASC; break;
+    }
+    this.setState({
+      order,
+      orderBy
+    });
+  }
+
+
+  render(){
+    const { classes } = this.props;
+    const { page, rowsPerPage, order, orderBy, nodes } = this.state;
+
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHeader
+            sortNodes={this.sortNodes}
+            headers={TABLE_HEADER}
+            orderBy={orderBy}
+            order={order}
+          />
+          <TableContent
+            data={nodes}
+            order={order}
+            orderBy={orderBy}
+            page={page}
+            rowsPerPage={rowsPerPage}
+            colNum={TABLE_HEADER.length}
+          />
+        </Table>
+        <TablePagination
+            component="div"
+            count={nodes.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            backIconButtonProps={{
+              'aria-label': 'Previous Page',
+            }}
+            nextIconButtonProps={{
+              'aria-label': 'Next Page',
+            }}
+            onChangePage={this.handleChangePage}
+            onChangeRowsPerPage={this.handleChangeRowsPerPage}
+          />
+      </Paper>
+    );
+  }
 }
 
 NodesTable.propTypes = {
   classes: PropTypes.object.isRequired,
+  nodes: PropTypes.array.isRequired,
 };
 
 export default withStyles(styles)(NodesTable);
