e125a59b00e1a5c01e06b0fc7aab4ae815a25106
Guglielmo Turco <guglielmo.turco@gmail.com>
2018-08-25 00:07:54 -0700

    Refacroring

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-const styles = {
+const styles = theme => ({
   root: {
-    flexGrow: 1,
+    zIndex: theme.zIndex.drawer + 1,
   },
-};
+
+});
 
 const Header = (props) => {
   const { classes } = props;
   return (
-    <div className={classes.root}>
-      <AppBar position="fixed">
+      <AppBar
+        position="fixed"
+        color="primary"
+        className={classes.root}
+      >
         <Toolbar>
           <Typography variant="title" color="inherit">
             Nessus
           </Typography>
         </Toolbar>
       </AppBar>
-    </div>
   );
 }
 
